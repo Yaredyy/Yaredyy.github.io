66c949624b419f3aad49403a08fd80fc65b50757
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2021-12-29 01:21:12 -0500

    updated the sucess to instead echo, much better than a new page

--- email_form.php
+++ email_form.php
@@ -20,30 +20,9 @@ $headers = "From: $email_address \r\n";
 $sent = mail($email_to, $email_subject, $message, $headers);
 
 
-if ($sent) {
-
-?><html>
-<head>
-<title>Your email was sent!</title>
-</head>
-<body>
-<h1>All Sent!</h1>
-<p>Thank you for your message.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>WOAH Didnt Work!</title>
-</head>
-<body>
-<h1>Something went wrong on the request!</h1>
-<p>We could not send your feedback. Please try again later.</p>
-</body>
-</html>
-<?php
-}
+if ($sent){
+       echo "success";
+    }else{
+        echo "invalid";
+    } 
 ?>
