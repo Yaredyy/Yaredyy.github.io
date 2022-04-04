0bae1adfd15282232f7edb1f3eb70159119919e9
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-04 02:31:41 -0400

    js name color, hope it works

---
+++ index.js
@@ -0,0 +1,13 @@
+class index {
+  colors=["Red","Blue","Yellow","Green","Orange","Purple","Pink","Black","White","Brown","Gray"];
+  flag = True;
+
+  constructor(){}
+
+  random(){
+    while(flag==True){
+      num = Math.floor(Math.random() * 11);
+      Document.getElementByID("name").style.color = colors[num];
+    }
+  }
+}
