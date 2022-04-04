ff9c5b57de5014db832bae74009bd1c5002683ab
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-04 02:37:03 -0400

    it didnt

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 class index {
-  colors=["Red","Blue","Yellow","Green","Orange","Purple","Pink","Black","White","Brown","Gray"];
-  flag = True;
+  let colors=["Red","Blue","Yellow","Green","Orange","Purple","Pink","Black","White","Brown","Gray"];
+  let flag = True;
 
   constructor(){}
 
   random(){
     while(flag==True){
-      num = Math.floor(Math.random() * 11);
+      let num = Math.floor(Math.random() * 11);
       Document.getElementByID("name").style.color = colors[num];
     }
   }
