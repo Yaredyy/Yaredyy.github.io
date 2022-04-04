a3a653e5458295e850920af0777e9a8e6c5a9b0e
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-04 04:02:31 -0400

    Update Color.js

--- Color.js
+++ Color.js
@@ -1,10 +1,10 @@
 
-  let colors=["Red","Blue","Yellow","Green","Orange","Purple","Pink","Black","White","Brown","Gray"];
+  let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
 
   const name = document.getElementById("name");
 
   name.addEventListener('mouseover', function handleMouseOver() {
-    name.style.color = "red";
+    name.style.color = colors[Math.floor(Math.random()*11)];
   });
   
   name.addEventListener('mouseout', function handleMouseOut() {
