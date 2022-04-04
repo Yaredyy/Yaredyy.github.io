c672891cfa560e26aeffefaf0e15baa8f2be4275
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-04 03:51:58 -0400

    this id

--- Color.js
+++ Color.js
@@ -2,7 +2,7 @@
   let colors=["Red","Blue","Yellow","Green","Orange","Purple","Pink","Black","White","Brown","Gray"];
   let flag = True;
 
-  random(this);{
+  random(id);{
       let num = Math.floor(Math.random() * 11);
-      this.style.color = colors[num];
+      id.style.color = colors[num];
   }
