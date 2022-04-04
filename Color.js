8961135c14cf87d6d387dbdb77b3b4c671cd8b37
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-04 03:54:55 -0400

    rewrite

--- Color.js
+++ Color.js
@@ -2,7 +2,7 @@
   let colors=["Red","Blue","Yellow","Green","Orange","Purple","Pink","Black","White","Brown","Gray"];
   let flag = True;
 
-  random(id);{
+  random();{
       let num = Math.floor(Math.random() * 11);
-      id.style.color = colors[num];
+      document.getElementById("name").style.color = colors[num];
   }
