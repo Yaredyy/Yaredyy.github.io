2a3e1def1f5472ad70f87005f0cfe0e8de44a92c
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-05 02:08:04 -0400

    new js

--- Color.js
+++ Color.js
@@ -1,12 +1,12 @@
 
   let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
 
-  const name = document.getElementById("name");
+  const name1 = document.getElementById("name");
 
-  name.addEventListener('mouseover', function handleMouseOver() {
-    name.style.color = colors[Math.floor(Math.random()*11)];
+  name1.addEventListener('mouseover', function handleMouseOver() {
+    name1.style.color = "red";
   });
   
-  name.addEventListener('mouseout', function handleMouseOut() {
-    name.style.color = colors[Math.floor(Math.random()*11)];
+  name1.addEventListener('mouseout', function handleMouseOut() {
+    name1.style.color = "white";
   });
