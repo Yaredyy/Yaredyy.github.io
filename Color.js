a67534d2e434c277b1df0b214f36f43a9a63cb7a
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-06 02:00:08 -0400

    Update Color.js

--- Color.js
+++ Color.js
@@ -1,12 +1,20 @@
 
   let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
-  var flag = 1;
 
   function Rainbow(){
     document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
   }
 
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
   while(flag==1){
-    setTimeout(() => { Rainbow(); }, 500);
+    sleep(500);
+    Rainbow();
   }
 
