b81fea64bd7135d39fc3ae8c0202779fe97ea37b
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-06 02:02:45 -0400

    recursive

--- Color.js
+++ Color.js
@@ -1,10 +1,6 @@
 
   let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
 
-  function Rainbow(){
-    document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
-  }
-
   function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -12,9 +8,11 @@
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
   }
-
-  while(flag==1){
+  function Rainbow(){
+    document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
     sleep(500);
     Rainbow();
   }
+
+  
 
