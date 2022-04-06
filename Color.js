23d59ba087b2da599d251c6afaa87a397162a10f
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-05 23:05:09 -0400

    fix

--- Color.js
+++ Color.js
@@ -1,14 +1,12 @@
 
-  // let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
+  let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
 
   const name1 = document.getElementById("name1");
 
   function Hovered(){
-    console.log("Check1");
-    name1.innerHTML = "I'm Yared Yehualashet";
+    name1.style.color = colors[Math.floor(Math.random * 11)];
   }
 
   function Unhovered(){
-    console.log("Check2");
-    name1.innerHTML = "I'm Yared Yohannes Yehualashet";
+    name1.innerHTML = colors[Math.floor(Math.random * 11)];
   }
