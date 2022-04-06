f0af05d5354d78fdc2361354b752e1dacc1e9664
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-05 23:33:35 -0400

    Update Color.js

--- Color.js
+++ Color.js
@@ -1,12 +1,10 @@
 
   let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
 
-  const name1 = document.getElementById("name1");
-
   function Hovered(){
-    name1.style.color = colors[Math.floor(Math.random * 11)];
+    document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
   }
 
   function Unhovered(){
-    name1.style.color = colors[Math.floor(Math.random * 11)];
+    document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
   }
