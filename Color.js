fd1a63de5a7e8d13ab0ffe48ba3d265c76adb37d
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-06 01:51:05 -0400

    var

--- Color.js
+++ Color.js
@@ -1,12 +1,12 @@
 
   let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
-  let flag = True;
+  var flag = 1;
 
   function Rainbow(){
     document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
   }
 
-  while(flag==True){
-    setTimeout(Rainbow, 500);
+  while(flag==1){
+    setTimeout(Rainbow(), 500);
   }
 
