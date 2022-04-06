136d3c45e18a27659bc3a0e11546d6047536ff12
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-06 19:58:44 -0400

    no black  and didnt push?

--- Color.js
+++ Color.js
@@ -1,23 +1,21 @@
 
-  let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
+  let colors=["red","blue","yellow","green","orange","purple","pink","white","brown","gray"];
 
   let coders = ["Backend Coder", "Frontend Coder", "Fullstack Coder", "Backend And Frontend Coder"];
   let code = 0;
 
-  let name1 = document.getElementById("name1");
+  // function sleep(milliseconds) {
+  //   const date = Date.now();
+  //   let currentDate = null;
+  //   do {
+  //     currentDate = Date.now();
+  //   } while (currentDate - date < milliseconds);
+  // }
 
-  function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
   function Rainbow(){
-    document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
+    document.getElementById("name").style.color = colors[Math.floor(Math.random()*10)];
   }
-  
-  name1.addEventListener()
+
 
   function endCoder(){
     document.getElementById("maxing").innerHTML = coders[code];
