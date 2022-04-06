f5bfc0a27743392fbed9b8fbc829f5b76bee0a6f
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-06 19:46:04 -0400

    coder and front

--- Color.js
+++ Color.js
@@ -1,6 +1,11 @@
 
   let colors=["red","blue","yellow","green","orange","purple","pink","black","white","brown","gray"];
 
+  let coders = ["Backend Coder", "Frontend Coder", "Fullstack Coder", "Backend And Frontend Coder"];
+  let code = 0;
+
+  let name1 = document.getElementById("name1");
+
   function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -10,9 +15,16 @@
   }
   function Rainbow(){
     document.getElementById("name1").style.color = colors[Math.floor(Math.random()*11)];
-    sleep(500);
-    Rainbow();
   }
+  
+  name1.addEventListener()
 
+  function endCoder(){
+    document.getElementById("maxing").innerHTML = coders[code];
+    code++;
+    if(code==4){
+      code=0;
+    }
+  }
   
 
