e612591c62a08d1424eca791e9ff079728943e17
Yaredyy <Yaredyyehualashet@gmail.com>
2022-05-27 18:51:18 -0400

    Update Color.js

--- Color.js
+++ Color.js
@@ -4,13 +4,6 @@
   let coders = ["Backend Coder", "Frontend Coder", "Fullstack Coder", "Backend And Frontend Coder"];
   let code = 0;
 
-  // function sleep(milliseconds) {
-  //   const date = Date.now();
-  //   let currentDate = null;
-  //   do {
-  //     currentDate = Date.now();
-  //   } while (currentDate - date < milliseconds);
-  // }
 
   function Rainbow(){
     document.getElementById("name").style.color = colors[Math.floor(Math.random()*10)];
