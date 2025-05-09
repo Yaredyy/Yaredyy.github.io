cbe0f2c244778f4a4205bf6194e7a0aeec06cef5
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2025-05-09 16:08:21 -0400

    Fixed errors
    Linked websites were down, so I aquired my own free images and set the images to local, as well as fixing cs and js errors because of the implementation of the images

--- like_button.js
+++ like_button.js
@@ -2,21 +2,23 @@ const e = React.createElement;
 
 var googleForms = document.createElement('div');
 googleForms.setAttribute('class', 'post block bc2');
-googleForms.innerHTML= '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQkjKQ2tehGLyViZEctWvr4ZdAYg38LKhedTHPM2NVvUYmMA/viewform?embedded=true" width="640" height="680" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>';
+googleForms.innerHTML = '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQkjKQ2tehGLyViZEctWvr4ZdAYg38LKhedTHPM2NVvUYmMA/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>';
 const domContainer = document.querySelector('#like_button_container');
+
 
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    
     this.state = { liked: false };
   }
 
+
   render() {
     if (this.state.liked) {
-      domContainer.appendChild(googleForms);
-      domContainer.scrollIntoView();
-      return;
+      if (!domContainer.contains(googleForms)) {
+        domContainer.appendChild(googleForms);
+      }
+      return null;
     }
 
     return e(
